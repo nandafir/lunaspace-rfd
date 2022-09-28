d7c71260ae21b4a70581fcb16077509e35ffd0c7
nandafir <nanda.fitraw@gmail.com>
2022-09-29 00:14:47 +0700

    setup tailwind

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+### source 
+[https://drive.google.com/drive/folders/1Vol63aqxP3l7t-bhQe2XyVu59SsaBwHm]
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "assets/css/app.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-xl font-bold">Hero</h1>
+      <button className="bg-blue-500 text-white font-medium px-8 py-3 rounded-md hover:bg-blue-700 duration-500">
+        SUBMIT
+      </button>
     </div>
   );
 }
